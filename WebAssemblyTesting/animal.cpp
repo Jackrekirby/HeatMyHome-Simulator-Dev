67186c745689076f66a9eedc81c9b493c4356eee
Jack Kirby <jackrekirby@gmail.com>
2021-10-22 23:44:33 +0100

    gitignore non cpp and header files

--- WebAssemblyTesting/animal.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "animal.h"
-
-Animal::Animal(int age)
-    : age(age)
-{
-    std::cout << "age: " << age << "\n";
-}
--- WebAssemblyTesting/animal.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Animal
-{
-public:
-    int age;
-
-    Animal(int age);
-};
